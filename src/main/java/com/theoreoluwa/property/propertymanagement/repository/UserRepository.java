f9020abd8d23a57dbd5ced0c21856452229b97cf
Theoreoluwa <ireeyy35@gmail.com>
2025-10-16 22:31:49 +0100

    Revert "Auth and Error Handling Implementation"
    This reverts commit 3b46e1a84db6e2e2d4ee83fdc6d693a6a4738ae8.

--- src/main/java/com/theoreoluwa/property/propertymanagement/repository/UserRepository.java
+++ src/main/java/com/theoreoluwa/property/propertymanagement/repository/UserRepository.java
@@ -3,10 +3,6 @@ package com.theoreoluwa.property.propertymanagement.repository;
 import com.theoreoluwa.property.propertymanagement.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends CrudRepository<UserEntity, Long>{
 
-    Optional<UserEntity> findByOwnerEmailAndPassword(String email, String password);
-    Optional<UserEntity> findByOwnerEmail(String email);
 }
