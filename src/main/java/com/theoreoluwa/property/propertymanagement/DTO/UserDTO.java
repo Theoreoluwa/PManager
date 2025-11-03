da31202197d731ed79fd44b589a4d1579f42652f
Theoreoluwa <ireeyy35@gmail.com>
2025-11-03 16:37:11 +0000

    Revert "Implementation of logging using logback and slf4j"
    This reverts commit a9e21a6ccf56be3bd4fbf66c755944ea95cd3616.

--- src/main/java/com/theoreoluwa/property/propertymanagement/DTO/UserDTO.java
+++ src/main/java/com/theoreoluwa/property/propertymanagement/DTO/UserDTO.java
@@ -3,9 +3,6 @@ package com.theoreoluwa.property.propertymanagement.DTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,12 +13,7 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
-    @NotNull(message = "Email is mandatory")
-    @NotEmpty(message = "Email cannot be empty")
-    @Size(min = 1, max= 50, message = "Email should be 1-50 characters")
     private String ownerEmail;
     private String phone;
-    @NotNull(message = "Password is mandatory")
-    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
